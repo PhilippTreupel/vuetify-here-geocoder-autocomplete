fa682b2e1687dfcde5f3c0946e4d614aced55b55
Philipp Treupel <philipp.treupel@gmail.com>
2021-01-02 09:04:30 +0100

    added VHereGeocoderAutocomplete component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,122 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+        <v-app-bar-title>Vuetify Here Geocoder Autocomplete</v-app-bar-title>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Github</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Here Geocoder API</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row
+          ><v-col cols="12" xs="12">
+            <v-card class="pa-8">
+              <v-row
+                ><v-col cols="12" xs="12">
+                  <div class="display-1 text-center">Usage Example:</div>
+                </v-col>
+                <v-col cols="12" xs="12">
+                  <v-here-geocoder-autocomplete
+                    :here-api-key="hereApiKey"
+                    v-model="place"
+                    debounce-time
+                    @input="print"
+                    clearable
+                  >
+                  </v-here-geocoder-autocomplete>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+          <v-col cols="12" xs="12">
+            <v-card class="pa-8">
+              <v-row
+                ><v-col cols="12" xs="12">
+                  <div class="display-1 text-center">Code:</div>
+                </v-col>
+                <v-col cols="12" xs="12">
+                  <code class="pa-3 mb-3 d-block">{{ installationCode }}</code>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import VHereGeocoderAutocomplete from "./components/VHereGeocoderAutocomplete";
 
 export default {
   name: "App",
+
   components: {
-    HelloWorld
+    VHereGeocoderAutocomplete
+  },
+
+  data: () => ({
+    hereApiKey: process.env.VUE_APP_HERE_API_KEY,
+    installationCode: "hi",
+    place: "Zaberfeld"
+  }),
+  methods: {
+    print() {
+      console.log(this.place);
+    },
+    customHighlight(placeItem) {
+      if (placeItem.highlights.title != null) {
+        let ret = placeItem.title;
+        // this is necessary because adding a bold tags displaces the highlight indexes delivered by the here api
+        let incrementCounter = 0;
+        placeItem.highlights.title.forEach(highlight => {
+          ret = [
+            ret.slice(0, highlight.start + incrementCounter),
+            "<b>",
+            ret.slice(
+              highlight.start + incrementCounter,
+              highlight.end + incrementCounter
+            ),
+            "</b>",
+            ret.slice(highlight.end + incrementCounter)
+          ].join("");
+          // number of characters added with the bold tag
+          incrementCounter += 7;
+        });
+        ret += "custom";
+        return ret;
+      } else {
+        return placeItem.title;
+      }
+    }
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/VHereGeocoderAutocomplete.vue
@@ -0,0 +1,308 @@
+<template>
+  <v-autocomplete
+    v-model="place"
+    v-bind="$attrs"
+    :items="places"
+    :loading="loading"
+    :search-input.sync="query"
+    :filter="filter"
+    return-object
+    item-text="title"
+    :prepend-icon="prependIcon"
+    v-on="$listeners"
+    @click:clear="onClear"
+  >
+    <template slot="item" slot-scope="data">
+      <template v-if="typeof data.item !== 'object'">
+        <v-list-item-content>{{ data.item }}</v-list-item-content>
+      </template>
+      <template v-else>
+        <v-list-item-content>
+          <slot name="highlight" :highlight="data.item.highlight">
+            <v-list-item-title v-html="data.item.highlight" />
+          </slot>
+        </v-list-item-content>
+      </template>
+    </template>
+  </v-autocomplete>
+</template>
+
+<script>
+export default {
+  name: "VHereGeocoderAutocomplete",
+  props: {
+    value: {
+      type: [Object, String],
+      required: false,
+      default() {
+        return {};
+      }
+    },
+    hereApiKey: {
+      type: String,
+      required: true
+    },
+    at: {
+      type: String,
+      required: false,
+      default: null
+    },
+    in: {
+      type: String,
+      required: false,
+      default: null
+    },
+    limit: {
+      type: Number,
+      required: false,
+      default: null
+    },
+    types: {
+      type: String,
+      required: false,
+      default: null
+    },
+    lang: {
+      type: String,
+      required: false,
+      default: navigator.language
+    },
+    resultType: {
+      type: String,
+      required: false,
+      default: null
+    },
+    houseNumberType: {
+      type: String,
+      required: false,
+      default: null
+    },
+    localityType: {
+      type: String,
+      required: false,
+      default: null
+    },
+    administrativeAreaType: {
+      type: String,
+      required: false,
+      default: null
+    },
+    debounceTime: {
+      type: [Boolean, Number, String],
+      default: false
+    },
+    // Vuetify props
+    prependIcon: {
+      type: String,
+      default: "mdi-map-marker"
+    },
+    customHighlight: {
+      type: Function,
+      default: placeItem => {
+        if (placeItem.highlights.title != null) {
+          let ret = placeItem.title;
+          // this is necessary because adding a bold tags displaces the highlight indexes delivered by the here api
+          let incrementCounter = 0;
+          placeItem.highlights.title.forEach(highlight => {
+            ret = [
+              ret.slice(0, highlight.start + incrementCounter),
+              "<b>",
+              ret.slice(
+                highlight.start + incrementCounter,
+                highlight.end + incrementCounter
+              ),
+              "</b>",
+              ret.slice(highlight.end + incrementCounter)
+            ].join("");
+            // number of characters added with the bold tag
+            incrementCounter += 7;
+          });
+          return ret;
+        } else {
+          return placeItem.title;
+        }
+      }
+    }
+  },
+  data() {
+    // The initial value can be a string or an object
+    // eslint-disable-next-line no-nested-ternary
+    const initialValue = this.value
+      ? typeof this.value === "string"
+        ? this.value
+        : this.value.title
+      : null;
+    const initialPlace = { title: initialValue };
+    return {
+      loading: false,
+      query: initialValue,
+      place: initialPlace,
+      places: initialValue ? [initialPlace] : [],
+      filter() {
+        return true; // filtering is done right after the api request
+      }
+    };
+  },
+  computed: {
+    searchQuery() {
+      let finalSearchQuery =
+        "https://autocomplete.search.hereapi.com/v1/autocomplete?q=";
+      if (this.query != null) {
+        finalSearchQuery += this.query.replace(/\s/g, "+");
+      }
+
+      finalSearchQuery += "&apiKey=" + this.hereApiKey;
+      if (this.at !== null) {
+        finalSearchQuery += "&at=" + this.at;
+      }
+      if (this.in !== null) {
+        finalSearchQuery += "&in=" + this.in;
+      }
+      if (this.limit !== null) {
+        finalSearchQuery += "&limit=" + this.limit;
+      }
+      if (this.types !== null) {
+        finalSearchQuery += "&types=" + this.types;
+      }
+      if (this.lang) {
+        finalSearchQuery += "&lang=" + this.lang;
+      }
+      return finalSearchQuery;
+    },
+    queryReady() {
+      return (
+        this.query !== null && this.query !== undefined && this.query !== ""
+      );
+    }
+  },
+  watch: {
+    query(val) {
+      if (val !== null && val !== undefined && val.length >= 1) {
+        this.debouncedSearchPlaces();
+      }
+    },
+    debounceTime: {
+      immediate: true,
+      handler(val) {
+        if (val) {
+          if (typeof val === "boolean") {
+            this.debouncedSearchPlaces = this.debounce(this.searchPlaces, 250);
+          } else if (typeof val === "number" || typeof val === "string") {
+            this.debouncedSearchPlaces = this.debounce(
+              this.searchPlaces,
+              Number(val)
+            );
+          }
+        } else {
+          this.debouncedSearchPlaces = this.searchPlaces;
+        }
+      }
+    },
+    searchQuery: {
+      deep: true,
+      handler(newVal, oldVal) {
+        const oldQuery = oldVal.split("?q=")[1].split("&")[0];
+        const newQuery = newVal.split("?q=")[1].split("&")[0];
+        if (newQuery !== "" && newQuery === oldQuery) {
+          this.debouncedSearchPlaces(place => {
+            this.place = place;
+          });
+        }
+      }
+    }
+  },
+  mounted() {
+    if (this.queryReady) {
+      this.searchPlaces(place => {
+        this.place = place;
+        this.$emit("input", this.place);
+      });
+    }
+  },
+  methods: {
+    async searchPlaces(callback = () => {}) {
+      this.loading = true;
+      try {
+        if (this.queryReady) {
+          let searchResults = await (await fetch(this.searchQuery)).json();
+          searchResults = searchResults.items;
+          if (searchResults !== null && searchResults !== undefined) {
+            this.loading = false;
+            if (this.resultType !== null) {
+              searchResults = searchResults.filter(item => {
+                return (
+                  item.resultType != null && item.resultType === this.resultType
+                );
+              });
+            }
+            if (this.localityType !== null) {
+              searchResults = searchResults.filter(item => {
+                return (
+                  item.localityType != null &&
+                  item.localityType === this.localityType
+                );
+              });
+            }
+            if (this.administrativeAreaType !== null) {
+              searchResults = searchResults.filter(item => {
+                return (
+                  item.administrativeAreaType != null &&
+                  item.administrativeAreaType === this.administrativeAreaType
+                );
+              });
+            }
+            if (this.houseNumberType !== null) {
+              searchResults = searchResults.filter(item => {
+                return (
+                  item.houseNumberType != null &&
+                  item.houseNumberType === this.houseNumberType
+                );
+              });
+            }
+            this.places = searchResults.map(item => {
+              const highlight = this.customHighlight(item);
+              delete item.highlights;
+              return {
+                ...item,
+                highlight
+              };
+            });
+            callback(this.places[0]);
+          } else {
+            this.loading = false;
+            callback(this.place);
+          }
+        } else {
+          this.loading = false;
+          callback(this.place);
+        }
+      } catch (error) {
+        this.loading = false;
+        this.$emit("error", error);
+      }
+    },
+    async debouncedSearchPlaces(callback = () => {}) {
+      return await this.searchPlaces(callback);
+    },
+    onClear() {
+      this.$emit("input", null);
+      this.$emit("clear");
+    },
+    debounce(callback, wait, immediate) {
+      let timeout;
+      return () => {
+        let context = this,
+          args = arguments;
+        let later = () => {
+          timeout = null;
+          if (!immediate) callback.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) callback.apply(context, args);
+      };
+    }
+  }
+};
+</script>
